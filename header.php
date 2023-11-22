df7cb944873a3bf202135a5041a4ca7c603135bf
Araygoza099 <araygoza099@gmail.com>
2023-11-22 12:21:11 -0600

    Index

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
                 </a>
             </li>
             <li><a href="servicios.php">Servicios</a></li>
-            <li><a href="portafolio.php">Portafolio</a></li>
+            <li><a href="acerdaDe.php">Acerca De</a></li>
             <li><a href="#">Contáctanos</a></li>
         </ul>
     </nav>
---
+++ inicio.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8">
+    <title>Inicio | Gam.co</title>
+    <link rel="stylesheet" type="text/css" href="style_index.css"> 
+    
+  </head>
+  <body >
+    <?php include('php\header.php'); ?>
+
+    
+    <p>Aqui surge la magia</p>
+
+      
+    <?php include('php\footer.php'); ?>
+
+  </body>
+</html>
+
+
