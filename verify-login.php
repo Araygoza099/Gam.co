5186aec8cbf5d6a8bcea713b22ad64e3b5a65cc1
DiegoEmmanuelMacias <diego.emm03@gmail.com>
2023-12-01 11:59:28 -0600

    Inicio de sesion

--- verify-login.php
+++ verify-login.php
@@ -81,10 +81,10 @@
 
         $conexion->close();
         if($band==1){
-            header('Location: alertas/success.html');
+            header('Location: alertas/loginOk.php');
         }
         else{
-            echo "algo no funciona";
+            header('Location: alertas/loginError.html');
         }
         header('Refresh: 1.5; location: login.php');
         exit; 
