3a27706bf8ffd6d00006d155bd4267eab5bfe32b
Christian Escamilla <capimarquez23@gmail.com>
2023-12-04 13:06:52 -0600

    Update tienda.php

--- tienda.php
+++ tienda.php
@@ -54,55 +54,61 @@
                 $categoriaSeleccionada = isset($_GET['categoria']) ? $_GET['categoria'] : 'opcion0';
 
                 // Muestra máximo 12 productos por página
-                $productsPerPage = 12;
-                $page = isset($_GET['page']) ? $_GET['page'] : 1;
-                $start = ($page - 1) * $productsPerPage;
-                $end = $start + $productsPerPage;
+$productsPerPage = 12;
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+$start = ($page - 1) * $productsPerPage;
+$end = $start + $productsPerPage;
 
-                foreach ($products as $product) {
-                    if ($categoriaSeleccionada === 'opcion0' || $product['type'] === $categoriaSeleccionada) {
-                        echo '<div class="product">';
-                    echo '<img src="img/base/' . $product["image"] . '" alt="' . $product["name"] . '">';
-                    echo '<h2';
+$counter = 0; // Variable para llevar el conteo de productos mostrados
 
-                    // Verificar si la cantidad disponible es cero
-                    if ($product["quantity"] == 0) {
-                        echo ' style="color: #999;"'; 
-                    }
+foreach ($products as $product) {
+    if ($categoriaSeleccionada === 'opcion0' || $product['type'] === $categoriaSeleccionada) {
+        if ($counter >= $start && $counter < $end) {
+            echo '<div class="product">';
+            echo '<img src="img/base/' . $product["image"] . '" alt="' . $product["name"] . '">';
+            echo '<h2';
 
-                    echo '>' . $product["name"] . '</h2>';
-                    
-                        $discountedPrice = $product["price"];
-                        if ($product["desc"] > 0) {
-                            $priceValue = $product["price"];
-                            $discountValue = $product["desc"];
-                            $discountedPrice = '$' . ($priceValue - ($priceValue * $discountValue / 100));
+            // Verificar si la cantidad disponible es cero
+            if ($product["quantity"] == 0) {
+                echo ' style="color: #999;"'; 
+            }
 
-                            echo '<p><span style="color: #999; text-decoration: line-through;"> $'. $product["price"] . '</span>';
-                            echo ' Precio: ' . $discountedPrice . '</p>';
+            echo '>' . $product["name"] . '</h2>';
+            
+            $discountedPrice = $product["price"];
+            if ($product["desc"] > 0) {
+                $priceValue = $product["price"];
+                $discountValue = $product["desc"];
+                $discountedPrice = '$' . ($priceValue - ($priceValue * $discountValue / 100));
 
-                        } else {
-                            if ($product["quantity"] == 0) {
-                                echo '<p><span style="color: #999;">Precio: $' . $product["price"] . '</span></p>';
-                            } else {
-                                echo '<p>Precio: ' . $product["price"] . '</p>';
-                            }
-                        }
-                    
-                        if ($product["quantity"] == 0) {
-                            echo '<a>No Disponible</a>';
-                        } else {
-                            echo '<a href="#">Agregar al Carrito</a>';
-                        }
-                    
-                        echo '<br><div class="desc">';
-                        echo "<p id='description'>" . $product['descrip'] . "</p>";
-                        echo '</div>';
-                    
-                        // ... (Resto de la estructura de tu producto)
-                        echo '</div>';
-                    }             
-                }          
+                echo '<p><span style="color: #999; text-decoration: line-through;"> $'. $product["price"] . '</span>';
+                echo ' Precio: ' . $discountedPrice . '</p>';
+
+            } else {
+                if ($product["quantity"] == 0) {
+                    echo '<p><span style="color: #999;">Precio: $' . $product["price"] . '</span></p>';
+                } else {
+                    echo '<p>Precio: ' . $product["price"] . '</p>';
+                }
+            }
+            
+            if ($product["quantity"] == 0) {
+                echo '<a>No Disponible</a>';
+            } else {
+                echo '<a href="#">Agregar al Carrito</a>';
+            }
+            
+            echo '<br><div class="desc">';
+            echo "<p id='description'>" . $product['descrip'] . "</p>";
+            echo '</div>';
+            
+            // ... (Resto de la estructura de tu producto)
+            echo '</div>';
+        }
+        $counter++; // Incrementa el contador de productos mostrados
+    }             
+}
+    
                 ?>
             </section>
 
