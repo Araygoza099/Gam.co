69dca3a2787d2ceada3b1c143f32be96b9ca1794
julioCgtzmartin <julioCgtzmartin@hotmail.com>
2023-12-05 14:16:27 -0600

    formularioDeSus-CUPONinicio

---
+++ suscripcion.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style_suscrip.css">
+    <title>Suscripcion</title>
+</head>
+<body>
+    <div class="container">  
+        <form id="contactus" action="#aqui hace lo del email" method="post">
+          <h3>Suscribete!</h3>
+          <h4>Para recibir un cupon de regalo!</h4>
+          <fieldset>
+            <input placeholder="Direccion Email" type="email" tabindex="1" name="email" required autofocus>
+          </fieldset>
+          <fieldset>
+            <button name="submit" type="submit" id="contactus-submit" data-submit="...Sending">Suscribete Ahora</button>
+          </fieldset>
+        </form>
+       
+            <img src="img/ellie.png" alt="" class="imgEllie">
+        </div>
+    
+
+</body>
+</html>
