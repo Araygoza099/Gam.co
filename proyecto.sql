3edbbf2a54ebfff7c8cdcbc848d8807b89f43ae5
DiegoEmmanuelMacias <diego.emm03@gmail.com>
2023-12-04 21:38:06 -0600

    filtro de precio

--- proyecto.sql
+++ proyecto.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 03-12-2023 a las 00:34:35
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+-- Host: 127.0.0.1
+-- Generation Time: Dec 05, 2023 at 03:18 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `proyecto`
+-- Database: `proyecto`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `det_pedido`
+-- Table structure for table `det_pedido`
 --
 
 CREATE TABLE `det_pedido` (
@@ -32,55 +32,94 @@ CREATE TABLE `det_pedido` (
   `proc_id` int(13) NOT NULL,
   `pedido_id` int(10) NOT NULL,
   `detpedido_cantidad` int(255) NOT NULL,
-  `prec_unitario` int(255) NOT NULL
+  `prec_unitario` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `envios`
+-- Table structure for table `direccion`
+--
+
+CREATE TABLE `direccion` (
+  `dir_id` int(5) NOT NULL,
+  `usr_id` int(5) NOT NULL,
+  `calle` varchar(255) NOT NULL,
+  `fracc` varchar(255) NOT NULL,
+  `zipcode` int(5) NOT NULL,
+  `estado` varchar(255) NOT NULL,
+  `ciudad` varchar(255) NOT NULL,
+  `num_tel` bigint(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `direccion`
+--
+
+INSERT INTO `direccion` (`dir_id`, `usr_id`, `calle`, `fracc`, `zipcode`, `estado`, `ciudad`, `num_tel`) VALUES
+(1, 2, 'El capulin 197', 'Residencial del Parque', 20277, 'Aguascalientes', 'Aguascalientes', 4494673952);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `envios`
 --
 
 CREATE TABLE `envios` (
   `envio_id` int(13) NOT NULL,
+  `dir_id` int(5) NOT NULL,
   `pedido_id` int(13) NOT NULL,
-  `envio_dir` text NOT NULL,
   `edo_envio` varchar(255) NOT NULL,
-  `envio_dateginal` date NOT NULL
+  `envio_datefinal` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `pagos`
+-- Table structure for table `pagos`
 --
 
 CREATE TABLE `pagos` (
   `pago_id` int(13) NOT NULL,
   `usr_id` int(5) NOT NULL,
   `card_id` int(13) NOT NULL,
-  `card_number` int(255) NOT NULL,
+  `card_number` varchar(255) NOT NULL,
   `card_thought` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pagos`
+--
+
+INSERT INTO `pagos` (`pago_id`, `usr_id`, `card_id`, `card_number`, `card_thought`) VALUES
+(0, 1, 0, '0000000000000000', '2030-01-01');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `pedidos`
+-- Table structure for table `pedidos`
 --
 
 CREATE TABLE `pedidos` (
   `pedido_id` int(13) NOT NULL,
   `usr_id` int(5) NOT NULL,
   `pago_id` int(13) NOT NULL,
-  `pedido_date` date NOT NULL,
-  `edo_pedido` varchar(255) NOT NULL
+  `total` bigint(255) NOT NULL,
+  `pagado` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pedidos`
+--
+
+INSERT INTO `pedidos` (`pedido_id`, `usr_id`, `pago_id`, `total`, `pagado`) VALUES
+(1, 2, 0, 0, 0),
+(2, 1, 0, 0, 0);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `productos`
+-- Table structure for table `productos`
 --
 
 CREATE TABLE `productos` (
@@ -95,24 +134,33 @@ CREATE TABLE `productos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `productos`
+-- Dumping data for table `productos`
 --
 
 INSERT INTO `productos` (`proc_id`, `proc_name`, `proc_descrip`, `proc_desc`, `proc_price`, `cantidad`, `proc_urlimg`, `type`) VALUES
-(1, 'Resident Evil 4 Remake', 'Videojuego clásico remasterizado para la nuevas consolas', 10, 1800, 30, 'img1.1.jpg', 'Videojuego'),
-(2, 'Red Dead Redemption 2', '', 10, 1200, 10, 'img1.2.jpeg', 'Videojuego'),
-(3, 'Minecraft', 'Video juego Minecraft', 20, 700, 10, 'img1.3.png', 'Videojuego'),
-(4, 'Cuphead', 'Videojuego perteneciente al género de corre y dispara, publicado por la empresa canadiense StudioMDHR', 10, 1200, 7, 'img1.4.jpg', 'Videojuego'),
-(5, 'Control Xbox Series X|S', 'Mando de Xbox color Blanco', 5, 1800, 100, 'img2.1.webp', 'Accesorio'),
-(6, 'Control Xbox Series X|S Camuflaje Rojo', '', 5, 1000, 10, 'img2.2.webp', 'Accesorio'),
-(7, 'Control Xbox Series X|S Azul', '', 5, 1000, 10, 'img2.3.jpg', 'Accesorio'),
-(8, 'Control Xbox Series X|S Negro', 'Mando de Xbox color Negro', 5, 1000, 10, 'img2.4.webp', 'Accesorio'),
-(9, 'Zuriel Said Zúñiga Delgadillo', 'a', 99, 100, 1, 'profileAvatar.png', 'Videojuego');
+(1, 'Resident Evil 4 - Remake', 'Seis años después de la catástrofe en Raccoon City, el agente Leon S. Kennedy es enviado a rescatar a la hija del presidente, secuestrada en una apartada población europea. La historia sigue un arriesgado rescate lleno de terror, explorando la intersecció', 10, 1299, 10, 'ResidentEvil4.webp', 'Videojuegos'),
+(2, 'Red Dead Redemption 2', 'Arthur Morgan y la banda de Van der Linde son forajidos buscados. Con agentes federales y los mejores cazarrecompensas pisándoles los talones, la banda deberá abrirse camino por el salvaje territorio del corazón de Estados Unidos a base de robos y peleas ', 50, 1300, 50, 'img1.2.jpeg', 'Videojuegos'),
+(3, 'Minecraft', 'Explora mundos generados al azar y construye cosas increíbles, desde la más humilde de las casas hasta el más majestuoso de los castillos. Juega en el modo creativo con recursos ilimitados o extrae en las profundidades del mundo, crea armas y armaduras pa', 15, 350, 0, 'img1.3.png', 'Videojuegos'),
+(4, 'Cuphead', 'Juega como Cuphead o Mugman (en modo de un jugador o cooperativo) y cruza mundos extraños, adquiere nuevas armas, aprende poderosos supermovimientos y descubre secretos ocultos mientras procuras saldar tu deuda con el diablo.', 30, 400, 5, 'img1.4.jpg', 'Videojuegos'),
+(5, 'Dragon Ball: Xenoverse 2', 'Dragon Ball Xenoverse 2 es la experiencia de juego de Dragon Ball definitiva cargada de acción emocionante, batallas épicas y opciones de personalización infinitas.', 0, 340, 0, 'DragonBallX.webp', 'Videojuegos'),
+(6, 'Cyberpunk 2077', 'Cyberpunk 2077 es un RPG de acción y aventura de mundo abierto ambientado en el futuro sombrío de Night City, una peligrosa megalópolis obsesionada con el poder, el glamur y las incesantes modificaciones corporales.', 0, 1500, 50, 'img1.5.jpg', 'Videojuegos'),
+(7, 'Injustice 2', 'En INJUSTICE 2, los combates moldean a los héroes de DC al otorgarles equipo personalizable. Mientras Batman enfrenta el régimen de Superman, una nueva amenaza pone en peligro la Tierra, desencadenando una épica lucha por la supervivencia.', 90, 1200, 50, 'Injustice.webp', 'Videojuegos'),
+(8, 'God of War: Ragnarok (Deluxe Edition Numerique)', 'Kratos y Atreus deben viajar a cada uno de los nueve reinos en búsqueda de respuestas, mientras que las fuerzas asgardianas se preparan para una batalla profetizada que terminará con el mundo.', 0, 1900, 0, 'GOW.jpeg', 'Videojuegos'),
+(9, 'Control Xbox ONE Elite Black Series 2', 'Adapta fácilmente este control Elite Series 2 para Xbox ONE, con los nuevos joysticks y palancas intercambiables estas características son las que hacen de este control para Xbox ONE tan único, porque al ser adaptable, podrá cubrir las necesidades de cual', 10, 2999, 50, 'img2.4.webp', 'Accesorios'),
+(10, 'Control Inalámbrico Xbox Serie S Y X Elite Series 2 Blanco', 'Diseñado para satisfacer las necesidades básicas de los jugadores competitivos de hoy en día, incluye solo los componentes que necesitas para desatar tu mejor juego. Incluye: Control, herramienta para ajustar palancas, cable de carga y guía de uso.', 0, 3000, 50, 'img2.1.webp', 'Accesorios'),
+(11, 'Xbox Wireless Controller - Shock Blue', 'El control inalámbrico de Xbox en color Azul ofrece un diseño ergonómico, con superficies esculpidas y textura para mayor comodidad y precisión. Destaca por su botón de Compartir para capturar contenido, la personalización de botones con la app Accesorios', 36, 1699, 0, 'img2.3.jpg', 'Accesorios'),
+(13, 'Playstation Mando Inalámbrico Ps5 Dualsense Camuflage Plateado', 'Con el innovador mando inalámbrico DualSense de PlayStation 5, podrás disfrutar de una experiencia de juego más inmersiva y profunda.', 15, 1375, 50, 'mandoPS5camuflageplateado.webp', 'Accesorios'),
+(14, 'Sony Playstation 5 DualSense Wireless Controller Cosmic Red', 'Este mando combina funciones revolucionarias mientras conserva precisión, comodidad y exactitud en cada movimiento. Gracias a su ergonomía especialmente pensada para la posición de tu mano, puedes pasar horas jugando con total confort. Mayor comodidad y r', 0, 1270, 0, 'mandoPS5cosmicred.webp', 'Accesorios'),
+(15, 'Control Inalámbrico PlayStation 5 DualSense Midnight Black', 'Explora nuevas fronteras de juego en tu PS5 con el Control Inalámbrico DualSense Midnight Black.', 65, 1530, 50, 'mandoPS5midnightblack.webp', 'Accesorios'),
+(16, 'Convertidor de Teclado', 'El adaptador de teclado y mouse KX es un producto innovador que permite a los jugadores disfrutar de juegos FPS en diferentes dispositivos con teclado USB y mouse.', 80, 399, 3, 'ConvetidorTeclado.jpg', 'Accesorios'),
+(18, 'Mortal Kombat 1', 'Descubre el nuevo universo de Mortal Kombat, creado por el Dios del Fuego Liu Kang. ¡Mortal Kombat 1 marca el comienzo de una nueva era para la icónica franquicia, con un sistema de lucha, modos de juego y Fatalities nuevos!', 0, 1200, 50, 'MK1.webp', 'Videojuegos'),
+(19, 'Soporte de Nintendo Switch Zelda Tears of The Kingdom', 'Este producto combina el diseño para las lágrimas del reino, y la apariencia es muy única y utiliza material ABS de alta calidad.', 5, 499, 15, 'SoporteNSzelda.jpg', 'Accesorios'),
+(20, 'Playstation Mando Inalámbrico Ps5 Dualsense Azul', 'Sony DualSense Galactic Purple - Mando Inalámbrico para PS5 Lo último en mandos inalámbricos para PlayStation está aquí. ', 5, 1239, 45, 'mandoPS5violeta.webp', 'Accesorios');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -126,19 +174,19 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`usr_id`, `username`, `email`, `password`, `intentos`, `pregunta`, `respuesta`) VALUES
-(0, 'admin', 'admin@gmail.com', 'password', 0, '', ''),
-(1, 'chriko', 'capimarquez23@gmail.com', '$2y$10$YYYsGkvFhqlWRUMYPoaq6O2uNhAgAHXko0Cta2PvfsZ/.mT.Yfxka', 1, 'equipo', 'chivas');
+(1, 'admin', 'admin@gmail.com', '$2y$10$UALFKSSAh1n8EO4TMqh1ie/N8omSOJXH0mr0a3wuYJkOSqBy2SjDO', 0, 'mascota', 'Zuriel'),
+(2, 'chris', 'capimarquez23@gmail.com', '$2y$10$BTFCh2ZYyvJ.spyHmgU3gOcXQKymdu17wCLlOsqTUnR6Dz/DgNJia', 0, 'mascota', 'diego');
 
 --
--- Índices para tablas volcadas
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `det_pedido`
+-- Indexes for table `det_pedido`
 --
 ALTER TABLE `det_pedido`
   ADD PRIMARY KEY (`detpedido_id`),
@@ -146,21 +194,29 @@ ALTER TABLE `det_pedido`
   ADD KEY `proc_id` (`proc_id`);
 
 --
--- Indices de la tabla `envios`
+-- Indexes for table `direccion`
+--
+ALTER TABLE `direccion`
+  ADD PRIMARY KEY (`dir_id`),
+  ADD KEY `usr_id` (`usr_id`);
+
+--
+-- Indexes for table `envios`
 --
 ALTER TABLE `envios`
   ADD PRIMARY KEY (`envio_id`),
-  ADD KEY `pedido_id` (`pedido_id`);
+  ADD KEY `pedido_id` (`pedido_id`),
+  ADD KEY `dir_id` (`dir_id`);
 
 --
--- Indices de la tabla `pagos`
+-- Indexes for table `pagos`
 --
 ALTER TABLE `pagos`
   ADD PRIMARY KEY (`pago_id`),
   ADD KEY `usr_id` (`usr_id`);
 
 --
--- Indices de la tabla `pedidos`
+-- Indexes for table `pedidos`
 --
 ALTER TABLE `pedidos`
   ADD PRIMARY KEY (`pedido_id`),
@@ -168,33 +224,53 @@ ALTER TABLE `pedidos`
   ADD KEY `pago_id` (`pago_id`);
 
 --
--- Indices de la tabla `productos`
+-- Indexes for table `productos`
 --
 ALTER TABLE `productos`
   ADD PRIMARY KEY (`proc_id`);
 
 --
--- Indices de la tabla `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`usr_id`);
 
 --
--- Restricciones para tablas volcadas
+-- Constraints for dumped tables
 --
 
 --
--- Filtros para la tabla `det_pedido`
+-- Constraints for table `det_pedido`
 --
 ALTER TABLE `det_pedido`
   ADD CONSTRAINT `det_pedido_ibfk_1` FOREIGN KEY (`pedido_id`) REFERENCES `pedidos` (`pedido_id`),
   ADD CONSTRAINT `det_pedido_ibfk_2` FOREIGN KEY (`proc_id`) REFERENCES `productos` (`proc_id`);
 
 --
--- Filtros para la tabla `envios`
+-- Constraints for table `direccion`
+--
+ALTER TABLE `direccion`
+  ADD CONSTRAINT `direccion_ibfk_1` FOREIGN KEY (`usr_id`) REFERENCES `users` (`usr_id`);
+
+--
+-- Constraints for table `envios`
 --
 ALTER TABLE `envios`
-  ADD CONSTRAINT `envios_ibfk_1` FOREIGN KEY (`pedido_id`) REFERENCES `pedidos` (`pedido_id`);
+  ADD CONSTRAINT `envios_ibfk_1` FOREIGN KEY (`pedido_id`) REFERENCES `pedidos` (`pedido_id`),
+  ADD CONSTRAINT `envios_ibfk_2` FOREIGN KEY (`dir_id`) REFERENCES `direccion` (`dir_id`);
+
+--
+-- Constraints for table `pagos`
+--
+ALTER TABLE `pagos`
+  ADD CONSTRAINT `pagos_ibfk_1` FOREIGN KEY (`usr_id`) REFERENCES `users` (`usr_id`);
+
+--
+-- Constraints for table `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD CONSTRAINT `pedidos_ibfk_1` FOREIGN KEY (`usr_id`) REFERENCES `users` (`usr_id`),
+  ADD CONSTRAINT `pedidos_ibfk_2` FOREIGN KEY (`pago_id`) REFERENCES `pagos` (`pago_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- tienda.php
+++ tienda.php
@@ -10,8 +10,8 @@
     <link rel="stylesheet" type="text/css" href="css/style_foo.css"> 
     <style>
         body {
-        overflow-x: hidden; 
-        background-image: url(img/fondo2.png);
+            overflow-x: hidden; 
+            background-image: url(img/fondo2.png);
         }
     </style>
 </head>
@@ -34,6 +34,14 @@
                     <option value="Accesorios">Accesorios</option>
                     <!-- Agrega más opciones según sea necesario -->
                 </select>
+                
+                <label for="filtro2">Precio</label>
+                <br>
+                <select id="filtro2" name="precio">
+                    <option value="mayor">Mayor->Menor</option>
+                    <option value="menor">Menor->Mayor</option>
+                </select>
+                
                 <button type="submit">Filtrar</button>
             </form>
         </aside>
@@ -49,7 +57,44 @@
             <section class="products">
                 <?php
                 // Simulación de productos desde una base de datos
-                require("productos.php"); 
+                $host = "127.0.0.1";
+                $username = "root";
+                $password = "";
+                $bd = "proyecto";
+
+                $con = new mysqli($host, $username, $password, $bd);
+
+                if ($con->connect_error) {
+                    die("Conexión fallida: " . $con->connect_error);
+                }
+
+                // array a actualizar
+                $products = array();
+
+                // Consulta SELECT
+                $sql = "SELECT proc_id, proc_name, proc_descrip, proc_desc, proc_price, cantidad, proc_urlimg, type FROM productos";
+                $result = $con->query($sql);
+
+                // procesa los resultados y actualiza el array
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        // añade cada fila al array
+                        $products[] = [
+                            "id" => $row["proc_id"],
+                            "name" => $row["proc_name"],
+                            "descrip" => $row["proc_descrip"],
+                            "desc" => $row["proc_desc"], 
+                            "price" => $row["proc_price"],
+                            "quantity" => $row["cantidad"],
+                            "image" => $row["proc_urlimg"],
+                            "type" => $row["type"]
+                        ];
+                    }
+                } else {
+                    echo "0 resultados";
+                }
+
+                $con->close();
 
                 $categoriaSeleccionada = isset($_GET['categoria']) ? $_GET['categoria'] : 'opcion0';
 
@@ -59,64 +104,83 @@
                 $start = ($page - 1) * $productsPerPage;
                 $end = $start + $productsPerPage;
 
+                $precioFinal = array(); // Array para almacenar los precios finales
+
                 $counter = 0; // Variable para llevar el conteo de productos mostrados
 
                 foreach ($products as $product) {
                     if ($categoriaSeleccionada === 'opcion0' || $product['type'] === $categoriaSeleccionada) {
+                        $precioFinal[$product['id']] = isset($product["price"]) ? $product["price"] : 0;
+
+                        if (isset($product["desc"]) && $product["desc"] > 0) {
+                            $precioFinal[$product['id']] = $precioFinal[$product['id']] - ($precioFinal[$product['id']] * $product["desc"] / 100);
+                        }
+
+                        $counter++;
+                    }
+                }
+
+                // Ordenar el array de precios según la opción seleccionada
+                if (isset($_GET['precio'])) {
+                    if ($_GET['precio'] === 'mayor') {
+                        arsort($precioFinal);
+                    } else {
+                        asort($precioFinal);
+                    }
+                }
+
+                $counter = 0; // Restablecer el contador
+
+                foreach ($precioFinal as $productId => $precio) {
+                    $product = isset($products[$productId]) ? $products[$productId] : null;
+                    if ($product && ($categoriaSeleccionada === 'opcion0' || $product['type'] === $categoriaSeleccionada)) {
                         if ($counter >= $start && $counter < $end) {
+                            // Resto del código para mostrar el producto
                             echo '<div class="product">';
-                            echo '<img src="img/base/' . $product["image"] . '" alt="' . $product["name"] . '">';
+                            echo '<img src="img/base/' . (isset($product["image"]) ? $product["image"] : '') . '" alt="' . (isset($product["name"]) ? $product["name"] : '') . '">';
                             echo '<h2';
 
                             // Verificar si la cantidad disponible es cero
-                            if ($product["quantity"] == 0) {
+                            if (isset($product["quantity"]) && $product["quantity"] == 0) {
                                 echo ' style="color: #999;"'; 
                             }
 
-                            echo '>' . $product["name"] . '</h2>';
+                            echo '>' . (isset($product["name"]) ? $product["name"] : '') . '</h2>';
                             
-                            $discountedPrice = $product["price"];
-                            if ($product["desc"] > 0) {
-                                $priceValue = $product["price"];
-                                $discountValue = $product["desc"];
-                                $discountedPrice = '$' . ($priceValue - ($priceValue * $discountValue / 100));
-
-                                echo '<p><span style="color: #999; text-decoration: line-through;"> $'. $product["price"] . '</span>';
+                            $discountedPrice = $precio;
+                            if (isset($product["desc"]) && $product["desc"] > 0) {
+                                echo '<p><span style="color: #999; text-decoration: line-through;"> $'. (isset($product["price"]) ? $product["price"] : '') . '</span>';
                                 echo ' Precio: ' . $discountedPrice . '</p>';
-                                $precioFinal = $discountedPrice;
-
                             } else {
-                                if ($product["quantity"] == 0) {
-                                    echo '<p><span style="color: #999;">Precio: $' . $product["price"] . '</span></p>';
+                                if (isset($product["quantity"]) && $product["quantity"] == 0) {
+                                    echo '<p><span style="color: #999;">Precio: $' . $precio . '</span></p>';
                                 } else {
-                                    echo '<p>Precio: ' . $product["price"] . '</p>';
+                                    echo '<p>Precio: ' . $precio . '</p>';
                                 }
-                                $precioFinal = $product["price"];
                             }
                             
-                            if ($product["quantity"] == 0) {
+                            if (isset($product["quantity"]) && $product["quantity"] == 0) {
                                 echo '<a>No Disponible</a>';
                             } else {
                                 // echo '<a href="verify-cart.php">Agregar al Carrito</a>';
                                 echo '<form action="verify-cart.php" method="post">';
-                                echo '<input type="hidden" name="productId" value="' . $product["id"] . '">';
-                                echo '<input type="hidden" name="productName" value="' . $product["name"] . '">';
-                                echo '<input type="number" name="cantidad" value="1" min="1" max="' . $product["quantity"] . '" style="display: block; font-family: \'Rubik\', sans-serif; text-align: center; padding: 8px 0px; margin-top: 15px; background-color: #333; color: #fff; text-decoration: none; border-radius: 5px; width: 100%; font-size: .95em;">'; // Input de tipo número con estilos CSS aplicados
-                                echo '<input type="hidden" name="precio" value="' . ($product["price"] - ($product["price"] * $product["desc"] / 100)) . '">';
+                                echo '<input type="hidden" name="productId" value="' . $productId . '">';
+                                echo '<input type="hidden" name="productName" value="' . (isset($product["name"]) ? $product["name"] : '') . '">';
+                                echo '<input type="number" name="cantidad" value="1" min="1" max="' . (isset($product["quantity"]) ? $product["quantity"] : 0) . '" style="display: block; font-family: \'Rubik\', sans-serif; text-align: center; padding: 8px 0px; margin-top: 15px; background-color: #333; color: #fff; text-decoration: none; border-radius: 5px; width: 100%; font-size: .95em;">';
+                                echo '<input type="hidden" name="precio" value="' . $precio . '">';
                                 echo '<button class="button-agregar" type="submit">Agregar al Carrito</button>';
                                 echo '</form>';
-
-
                             }
                             
                             echo '<br><div class="desc">';
-                            echo "<p id='description'>" . $product['descrip'] . "</p>";
+                            echo "<p id='description'>" . (isset($product['descrip']) ? $product['descrip'] : '') . "</p>";
                             echo '</div>';
                             
                             // ... (Resto de la estructura de tu producto)
                             echo '</div>';
                         }
-                        $counter++; // Incrementa el contador de productos mostrados
+
+                        $counter++;
                     }             
                 }
     
@@ -125,7 +189,7 @@
 
             <div class="pagination">
                 <?php
-                $totalPages = ceil(count($products) / $productsPerPage);
+                $totalPages = ceil(count($precioFinal) / $productsPerPage);
                 for ($i = 1; $i <= $totalPages; $i++) {
                     echo '<a href="?page=' . $i . '">' . $i . '</a>';
                 } ?>
@@ -136,5 +200,5 @@
     <div style="padding: 150px;"></div>
     <?php include('footer.php'); ?>
 
-    </body>
+</body>
 </html>
